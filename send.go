4e57a0d3e862b2d5e38182b46a117ba583b34704
Gregory Man <man.gregory@gmail.com>
2014-12-11 13:54:42 +0200

    Implemented sending timeout

--- send.go
+++ send.go
@@ -3,6 +3,11 @@ package main
 import (
 	"github.com/andybons/hipchat"
 	"log"
+	"time"
+)
+
+const (
+	timeout time.Duration = 2
 )
 
 func SendMessage(msg string, config Config) {
@@ -16,7 +21,21 @@ func SendMessage(msg string, config Config) {
 		Notify:        true,
 	}
 
-	if err := c.PostMessage(request); err != nil {
+	errc := make(chan error, 1)
+	okc := make(chan bool, 1)
+	go func() {
+		err := c.PostMessage(request)
+		if err != nil {
+			errc <- err
+		}
+		okc <- true
+	}()
+
+	select {
+	case <-okc:
+	case err := <-errc:
 		log.Printf("Cannot send message: %s %q", msg, err)
+	case <-time.After(time.Second * timeout):
+		log.Printf("Timout after %d seconds", timeout)
 	}
 }
