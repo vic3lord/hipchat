5c6ca66582766195c4cb68404e7a456e1144b7b3
Or Elimelech <0r3limelech@gmail.com>
2016-02-18 23:45:57 +0200

    go 1.6 vendor

---
+++ vendor/github.com/andybons/hipchat/examples/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+
+	"github.com/andybons/hipchat"
+)
+
+func main() {
+	c := hipchat.Client{AuthToken: "<PUT YOUR AUTH TOKEN HERE>"}
+	req := hipchat.MessageRequest{
+		RoomId:        "Rat Man’s Den",
+		From:          "GLaDOS",
+		Message:       "Bad news: Combustible lemons failed.",
+		Color:         hipchat.ColorPurple,
+		MessageFormat: hipchat.FormatText,
+		Notify:        true,
+	}
+	if err := c.PostMessage(req); err != nil {
+		log.Printf("Expected no error, but got %q", err)
+	}
+}
